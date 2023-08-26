59457b381acecf84b56ddff5486f5e7b48afcbc5
JuniorWizard <99333545+JuniorWizard@users.noreply.github.com>
2023-08-26 03:25:20 -0400

    Commenting Search.php
    Adding Logic Comments to the Search.php

--- API/Search.php
+++ API/Search.php
@@ -1,32 +1,33 @@
 <?php 
 require_once('../vendor/autoload.php');
-require_once('key.php');
-$query=$_POST['searchValue'];
+require_once('key.php'); // API Key
+$query=$_POST['searchValue']; // Search value sent via AJAX
 
-$client=new Google\Client();
-$client->setApplicationName("Client_Library_Examples");
-$client->setDeveloperKey($key);
+$client=new Google\Client(); // Google Library 
+$client->setApplicationName("INSERT_APPLICATION_NAME");
+$client->setDeveloperKey($key); //Assigning Developer Key
 
 $service=new Google\Service\Books($client);
-$results=$service->volumes->listVolumes($query);
-$response_package=array();
-foreach($results->getItems() as $items){
-    $volInfo=$items['volumeInfo'];
-    $title=$volInfo['title'];
-    $subtitle=$volInfo['subtitle'];
+$results=$service->volumes->listVolumes($query); //Executing Query and getting response in form of array
+//Rather than send a large array back I broke it down and only gathered the data I wanted to display / use.
+$response_package=array(); // Array I'm sending back to the index
+foreach($results->getItems() as $items){ // For each record in the array
+    $volInfo=$items['volumeInfo']; // Multidimensional Array
+    $title=$volInfo['title']; 
+    $subtitle=$volInfo['subtitle']; 
     $authors=$volInfo['authors'];
-    $dsecription=$volInfo['description'];
-    $averageRating=$volInfo['averageRating'];
-    if(isset($volInfo['imageLinks'])){
+    if(isset($volInfo['imageLinks'])){ // Ensure that the image isn't blank
         $image=$volInfo['imageLinks']['smallThumbnail'];
     }else{
         $image=null;
     }
     $link=$volInfo['infoLink'];
 
+    //Packaging up all the info
     $response_package[]=array('title'=>$title,'sub'=>$subtitle,'auth'=>$authors,'desc'=>$dsecription,'rate'=>$averageRating,'image'=>$image,'link'=>$link);
 }
 
+// Sending it back as a JSON array
 echo(json_encode($response_package));
 
 ?>
