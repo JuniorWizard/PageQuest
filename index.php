6ec36c927453b269804aecbbd6a5c6940f449964
JuniorWizard <99333545+JuniorWizard@users.noreply.github.com>
2023-08-26 03:07:24 -0400

    Adding Comments
    Added Logic Comments to my code.

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="en-US">
+<html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name='viewport' content='width=device-width,initial-scale=1.0'>
+        <meta name='viewpoint' content='width=device-width,initial-scale=1.0'>
         <title>PageQuest</title>
         <link rel='stylesheet' type='text/css' href='style/Main.css'>
         <script type='text/javascript' src='JQuery/JQuery.js'></script>
@@ -11,9 +11,9 @@
         <h2>PageQuest</h2>
         <div class="SearchContainer">
             <br>
-            <input type="text" id="Search" placeholder="Search Here">
+            <input type=text id='Search' placeholder="Search Here">
             <br><br>
-            <button class="button" type="button" id="ActionSearch">Search</button>
+            <button class='button' type=button id='ActionSearch'>Search</button>
             <br><br>
         </div>
         <div class='LoadContainer'>
@@ -27,61 +27,61 @@
             </div>
         </div>
         <script>
-            $(document).on('keypress',function(e){
+            $(document).on('keypress',function(e){ // Handling if the user presses the enter key rather than directly clicking the button
                 if(e.which ==13){  
-                    $("#ActionSearch").click();
+                    $("#ActionSearch").click(); 
                   
                 }
             })
        
-            $("#ActionSearch").on('click',function(){
-                $("#ActionSearch").addClass('Transition');
-                $("#Search").addClass('Transition');
-                $(".loader").addClass('load');
-                $("#loading_text").addClass('load');
-                    setTimeout(function(){
-                        $(".SearchContainer").addClass('Searched');
+            $("#ActionSearch").on('click',function(){ // When the search button is clicked .. do the following
+                $("#ActionSearch").addClass('Transition'); // Transition class to allow for CSS transition
+                $("#Search").addClass('Transition'); 
+                $(".loader").addClass('load'); // Adding Loading Animation class
+                $("#loading_text").addClass('load'); // Shows Loading Animation text
+                    setTimeout(function(){ // After 900 miliseconds do the following..
+                        $(".SearchContainer").addClass('Searched'); // CSS hides the main search div
                     },900)
-                var searchAction=$("#Search").val();
-                if(searchAction.length!==0){
-                    $.ajax({
+                var searchAction=$("#Search").val(); // Get searched for value
+                if(searchAction.length!==0){ // If the length of that value does not equal zero then ..
+                    $.ajax({ // AJAX Call .. POSTING Data to the URL
                         type:"POST",
-                        url:"API/Search.php",
+                        url:"API/Search.php", 
                         data:{
-                            searchValue:searchAction,
+                            searchValue:searchAction, 
                         },
-                        success:function(data){
-                          
-                           var e=JSON.parse(data);
-                           console.log(e);
-                           var count_of_entries=e.length;
-                           for(var i=0;i<count_of_entries;i++){
-                            var book_cover=e[i]['image'];
-                            var title=e[i]['title'];
-                            var link=e[i]['link'];
+                        success:function(data){ // If the post is successful : 
+                           var e=JSON.parse(data); // Parse Response into an array
+                           var count_of_entries=e.length; // Getting Length of array
+                           for(var i=0;i<count_of_entries;i++){ // For Loop to handle each record within the array
+                            var book_cover=e[i]['image']; // Book Cover Image
+                            var title=e[i]['title']; // Book Title
+                            var link=e[i]['link']; // Book Link
                             var auth=e[i]['auth'][0]; // Authors is an array need to check for amount of authors maybe ?
-                            BookDisplay_Template(book_cover,title,auth,link);
-                            setTimeout(function(){
-                                $("#loading_text").removeClass('load');
-                                $('.loader').removeClass('load');
+                            BookDisplay_Template(book_cover,title,auth,link); // Using Template Formula .. See below 
+                            setTimeout(function(){ // After 2000 miliseconds do the following ..
+                                $("#loading_text").removeClass('load'); // Hiding Load Text
+                                $('.loader').removeClass('load'); // Hiding Load Animation
                             },2000)
-                            setTimeout(function(){
-                                $("#Results").show();
+                            setTimeout(function(){  // This is intentionally, seperated from the above as this adds a slightly delay.
+                                $("#Results").show(); // Shows Results
                             },2000)
                            }
                         },
-                        error:function(data){
-
+                        error:function(data){ // If the post was not successful : 
+                            alert("Oh No ! An Error Occured, please refresh and try again.");
                         }
                     })
-                }else{
+                }else{ // If search value is empty:
                     alert("Please Search Value");
                 }
             })
-            function BookDisplay_Template(img,book_title,auth,move_link){
-                const mainContainer = $("#Results_Container");
-                var book_container = $('<div>', {
-                    class: 'book_container'
+            
+            //Book display Template that creates a single book elment. 
+            function BookDisplay_Template(img,book_title,auth,move_link){ 
+                const mainContainer = $("#Results_Container"); // Main Results Container
+                var book_container = $('<div>', { // Describe the html element that it's creating
+                    class: 'book_container' 
                 });
                 var imageLink = $('<a>', {
                     href: move_link,
@@ -89,7 +89,7 @@
                 });
                 var image = $('<img>', {
                     src: img || 'images/missing.png', // Default image if img is null
-                    alt: 'Book Cover',
+                    alt: 'Book Cover', 
                     class: 'book-img'
                 });
                 var details = $('<div>', {
@@ -100,6 +100,7 @@
                     text: book_title
                 });
 
+                // Appending those elements into their appropriate containers
                 details.append(title);
                 imageLink.append(image);
                 book_container.append(imageLink);
